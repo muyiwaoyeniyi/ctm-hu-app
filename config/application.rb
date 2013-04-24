c530b57b29910215965595c94427ca78ec2a9697
Taiwo <taiwo@taiwo-1215P.(none)>
2013-04-24 13:49:01 -0400

    Configured for deployment to Heroku

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,6 @@ module TransferApp
     config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    config.assets.version = '1.0' 
   end
 end
