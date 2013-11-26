6b53ee8b110453b825dbb0cf8fb699454b5661cc
Taiwo <taiwo@taiwo-1215P.(none)>
2013-11-26 01:14:34 -0500

    css fixes + basic caching

--- config/application.rb
+++ config/application.rb
@@ -57,5 +57,9 @@ module TransferApp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0' 
+
+    # Serving static assets and setting cache headers
+    #config.serve_static_assets = true
+    config.static_cache_control = "public, max-age=31536000"
   end
 end
